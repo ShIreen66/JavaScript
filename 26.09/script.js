08dcc021b4bd5834df88807c23b7503184ed974e
ShIreen66 <066shiree@gmail.com>
2024-09-26 20:32:04 +0530

    fat arrow

--- 26.09/script.js
+++ 26.09/script.js
@@ -1,5 +1,5 @@
 // default parameter
-// rest parameter
+
 
 function dets(firstname, batchcode = "MERN12"){
     console.log(firstname, batchcode);
@@ -12,3 +12,14 @@ dets("John1", "MERN12");
 
 //1. default parameter must be the last parameter of the function
 //2. there should only 1 default parameter in the function
+
+// rest (operator) parameter (...h)
+function hobbies(name, ...h) {
+    console.log(name, h);
+}
+hobbies("john", "swimming");
+hobbies("john", "swimming", "reading");
+hobbies("john");
+hobbies("john", "swimming", "reading", "singing");
+//1. rest operator must be the last parameter in the function
+//2. there must be only one rest parameter in the function
